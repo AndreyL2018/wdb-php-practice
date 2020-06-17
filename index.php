e0e3491fb778cc6295eb7ae3654ab91943919293
itstudent17 <49056212+itstudent17@users.noreply.github.com>
2020-06-17 20:20:10 +0300

    Index file added

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <form action="" method="POST">
+        <div class="form-group">
+            <h2>Registration</h2>
+            <input type="text" name="username" class="form-control" placeholder="Username" required>
+            <input type="email" name="email" class="form-control" placeholder="Email" required>
+            <input type="password" name="password" class="form-control" placeholder="Password" required>
+            <button type="submit" class="btn btn-lg btn-primary btn-block">Register</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
+
+<?php
